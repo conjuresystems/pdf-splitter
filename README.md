73270af1e507ed8d83577a944e78ea001c26d795
tlai <tlai@conjurers.ca>
2023-07-25 15:53:42 -0700

    Split by num characters, added README

---
+++ README.md
@@ -0,0 +1,4 @@
+# How to use
+1. put the source pdfs in the root directory
+2. run SplitBookToText.py (install the deps if havent)
+3. the output will be in ./output folder
--- SplitBookToText.py
+++ SplitBookToText.py
@@ -1,41 +1,34 @@
 import os
-import pdfplumber
+from PyPDF2 import PdfReader
+import re
 
-def convert_pdf_to_text(file, output_directory):
-    MAX_SIZE = 16 * 1024 * 1024  # 16MB
-    current_size = 0
-    file_count = 0
-    output_file = None
+def split_pdf_to_text(filepath, output_directory):
+    reader = PdfReader(filepath)
+    filename = os.path.splitext(os.path.basename(filepath))[0]
+    text = ""
+    for page in reader.pages:
+        text += page.extract_text()
+    chunks = re.split(r'(?<=[.!?])\s+', text)
+    chunk_text = ""
+    chunk_count = 1
+    for chunk in chunks:
+        if len(chunk_text + chunk) > 64000:
+            with open(f"{output_directory}/{filename}_chunk_{chunk_count}.md", "w", encoding="utf-8") as f:
+                f.write(chunk_text)
+            chunk_text = chunk
+            chunk_count += 1
+        else:
+            chunk_text += " " + chunk
+    if chunk_text:
+        with open(f"{output_directory}/{filename}_chunk_{chunk_count}.md", "w", encoding="utf-8") as f:
+            f.write(chunk_text)
 
-    with pdfplumber.open(file) as pdf:
-        for page in pdf.pages:
-            text = page.extract_text()
-            text = text.encode('utf-8')  # Convert to bytes to get accurate size
-
-            if current_size + len(text) > MAX_SIZE or output_file is None:
-                if output_file is not None:
-                    output_file.close()
-
-                output_filename = f"{os.path.splitext(os.path.basename(file))[0]}_chunk_{file_count}.txt"
-                output_filepath = os.path.join(output_directory, output_filename)
-                output_file = open(output_filepath, 'wb')
-
-                current_size = 0
-                file_count += 1
-
-            output_file.write(text)
-            current_size += len(text)
-
-        if output_file is not None:
-            output_file.close()
-
-def convert_pdfs_in_directory(directory):
-    output_directory = os.path.join(directory, 'output2')
-    os.makedirs(output_directory, exist_ok=True)
-
+def split_pdfs_in_directory(directory, output_directory):
     for filename in os.listdir(directory):
         if filename.endswith(".pdf"):
-            filepath = os.path.join(directory, filename)
-            convert_pdf_to_text(filepath, output_directory)
+            pdf_output_directory = os.path.join(output_directory, os.path.splitext(filename)[0])
+            if not os.path.exists(pdf_output_directory):
+                os.makedirs(pdf_output_directory)
+            split_pdf_to_text(os.path.join(directory, filename), pdf_output_directory)
 
-convert_pdfs_in_directory(os.getcwd())
+split_pdfs_in_directory(os.getcwd(), "output")
